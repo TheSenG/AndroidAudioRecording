43858837b3d179768e7a752123c65ddbdc914f3a
Edgar <edefusco.dam@institutcampalans.net>
2024-02-14 17:43:48 +0100

    Start Commit

---
+++ app/src/main/java/com/example/androidaudiorecording/HomescreenActivity.kt
@@ -0,0 +1,32 @@
+package com.example.androidaudiorecording
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class HomescreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homescreen)
+
+        // Hide action bar
+        supportActionBar?.hide()
+
+        // Text view
+        val backgroundText: TextView = findViewById(R.id.textView)
+        val textAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_in)
+        backgroundText.startAnimation(textAnimation)
+
+        // Splashscreen delay
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+            // Time in milliseconds
+        }, 6000)
+    }
+}
